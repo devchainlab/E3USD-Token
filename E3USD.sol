ce2186200dec1cb03df159e40a63230c6c0d6e21
Kirill <offkirillkuzin@gmail.com>
2018-07-04 00:22:44 +0300

    Initial commit

---
+++ E3USD.sol
@@ -0,0 +1,131 @@
+pragma solidity ^0.4.10;
+
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+     * account.
+     */
+    function Ownable() public {
+        owner = msg.sender;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /**
+     * @dev Allows the current owner to transfer control of the contract to a newOwner.
+     * @param newOwner The address to transfer ownership to.
+     */
+    function transferOwnership(address newOwner) onlyOwner public {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+
+}
+
+/**
+ * @title SafeMath
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+
+    function mul(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a * b;
+        assert(a == 0 || c / a == b);
+        return c;
+    }
+
+    function div(uint256 a, uint256 b) internal constant returns (uint256) {
+        // assert(b > 0); // Solidity automatically throws when dividing by 0
+        uint256 c = a / b;
+        // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+        assert(b <= a);
+        return a - b;
+    }
+
+    function add(uint256 a, uint256 b) internal constant returns (uint256) {
+        uint256 c = a + b;
+        assert(c >= a);
+        return c;
+    }
+
+}
+
+contract ERC20Basic {
+    uint256 public totalSupply;
+    function balanceOf(address who) public constant returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract BasicToken is ERC20Basic {
+
+    using SafeMath for uint256;
+
+    mapping(address => uint256) balances;
+
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+        Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function balanceOf(address _owner) constant public returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function () public payable {
+        revert();
+    }
+
+}
+
+contract MintableToken is BasicToken, Ownable {
+
+    event Mint(address indexed to, uint256 amount);
+
+    function mint(address _to, uint256 _amount) onlyOwner public returns (bool) {
+        totalSupply = totalSupply.add(_amount);
+        balances[owner] = balances[owner].add(_amount);
+        Mint(_to, _amount);
+        firstTransfer(_to, _amount);
+        return true;
+    }
+
+    function firstTransfer(address _to, uint256 _amount) onlyOwner private returns(bool) {
+        balances[owner] = balances[owner].sub(_amount);
+        balances[_to] = balances[_to].add(_amount);
+        Transfer(owner, _to, _amount);
+        return true;
+    }
+
+}
+
+contract E3USD is MintableToken {
+
+    string public constant name = "E3USD";
+    string public constant symbol = "E3U";
+    uint32 public constant decimals = 0;
+
+}
